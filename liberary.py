dffb173b2100163ede08924c214d032a8677fd4e
Alka Ragani <109142333+sysapptime@users.noreply.github.com>
2022-09-19 17:40:21 +0530

    input update db

--- liberary.py
+++ liberary.py
@@ -1,130 +1,160 @@
-
-#------------ TABLE CREATION QUERIES -----------#
-# query = 'CREATE TABLE student(ID INT PRIMARY KEY, name VARCHAR(50), father_name VARCHAR(100), roll INT, contact VARCHAR(12));'
-# cursor.execute(query)
-# db.commit()
-
-# query = 'CREATE TABLE add_book(ID INT PRIMARY KEY, book_name VARCHAR(50), publication_name VARCHAR(100), book_id INT, book_status VARCHAR(12));'
-# cursor.execute(query)
-# db.commit()
-
-# query = 'CREATE TABLE issue_book(ID INT PRIMARY KEY, book_name VARCHAR(50), student_name VARCHAR(100), student_roll INT, issue_date date);'
-# cursor.execute(query)
-# db.commit()
-
-# query = 'CREATE TABLE submit_book(ID INT PRIMARY KEY, book_name VARCHAR(50), student_name VARCHAR(100), roll INT, dos date);'
-# cursor.execute(query)
-# db.commit()
-
-#---------END OF TABLE CREATION QUERY -------------------#
-
-""" Input Function start """
-# IMPORTIG REQUIRED MODULE
 import sqlite3
+db = sqlite3.connect ('liberary.db' )
+cr = db.cursor()
 
-# DB CONNECTION
-db = sqlite3.connect("db.sqlite3")
-cursor = db.cursor()
-
-def addStudent():
-    name = input("Enter student name : ")
-    rollNo = int(input("Enter the roll of student :"))
-    father_name = input("enter father name :")
-    contact_no = (input("Enter contect number :"))
-
-    query = 'SELECT name, ID from student order by ID ASC;'
-    cursor.execute(query)
-    curStudent = cursor.fetchall()
-    id = curStudent[-1][1] + 1
-    for st in curStudent:
-        print(' BEfore Insertion Student Name are as ', st[0])
-    query = f'INSERT INTO student(ID, name, father_name, roll, contact) VALUES ({id}, "{name}", "{father_name}", {rollNo}, "{contact_no}")'
-    cursor.execute(query)
-    db.commit()
-    query = 'SELECT name from student order by ID ASC;'
-    cursor.execute(query)
-    curStudent = cursor.fetchall()
-
-    
-    print('After insertion Student Name are as ', curStudent[-1][0])
+class Liberary():
+      # CREATING INIT FUNCTION...........
+   # def __init__(self,name,roll,father_name,contact_no):
+   #    self.name = name
+   #    self.roll = roll
+   #    self.father_name = father_name
+   #    self.contact_no = contact_no
 
 
-# To issue a book from liberary.....
-def issue_book():
+          #...................... ADDING STUDENTS..........................
+   def addStudent(self):
 
-    Booknm = input("Enter the name of book: ")
-    Issuedate = input("Enter the date of issue book :")
-    Studentnm = input("enter the name of student :")
-    studentroll = int(input("Enter the roll of student :"))
-    
-    query = 'SELECT book_name, ID from issue_book order by ID ASC;'
-    cursor.execute(query)
-    curStudent = cursor.fetchall()
-    id = curStudent[-1][1] + 1
-    for st in curStudent:
-        print(' BEfore Insertion ISsue Book Name are as ', st[0])
-    query = f'INSERT INTO issue_book(ID, book_name, student_name, student_roll, issue_date) VALUES ({id}, "{Booknm}", "{Studentnm}", {studentroll}, "{Issuedate}")'
-    cursor.execute(query)
-    db.commit()
-    query = 'SELECT book_name, issue_date from issue_book order by ID ASC;'
-    cursor.execute(query)
-    curStudent = cursor.fetchall()
+      db = sqlite3.connect ('liberary.db' )
+      cr = db.cursor()
+      # Creating table for student
+      # query = 'CREATE TABLE student(ID INT PRIMARY KEY, Name varchar(100), father_name varchar(100), roll_no INT, contact_no int(12));'
+      # cr.execute(query)
+      # db.commit()
 
-    
-    print('After insertion ISsue book  Name are as ', curStudent[-1][0], curStudent[-1][1])
+      name = input("Enter student name : ")
+      rollNo = int(input("Enter the roll of student :"))
+      father_name = input("enter father name :")
+      contact_no = int(input("Enter contact number :"))
 
+      idQuery = 'SELECT  ID FROM student ORDER BY ID DESC;'
+      cr.execute(idQuery)
+      res = cr.fetchone()
+      # Query TO Insert Data in student Table 
+      print(res)
+      newID = res[0]+1
+      query = f'INSERT INTO student VALUES({newID},"{name}", "{father_name}" ,{rollNo},{contact_no});' 
+      cr.execute(query)
+      db.commit()
 
-# to submit a book in liberary.....
-def submitBook():
-    DOS = input("Date of submission : ")
-    bookName = input("Enter Book Name : ")
-    studentName = input("enter the name of student :")
-    studentroll = int(input("Enter the roll of student :"))
-    query = 'SELECT book_name, ID from submit_book order by ID ASC;'
-    cursor.execute(query)
-    curStudent = cursor.fetchall()
-    id = curStudent[-1][1] + 1
-    for st in curStudent:
-        print(' BEfore Insertion Submit Book Name are as ', st[0])
-    query = f'INSERT INTO submit_book(ID, book_name, student_name, roll, dos) VALUES ({id}, "{bookName}", "{studentName}", {studentroll}, "{DOS}")'
-    cursor.execute(query)
-    db.commit()
-    query = 'SELECT book_name, dos from submit_book order by ID ASC;'
-    cursor.execute(query)
-    curStudent = cursor.fetchall()
+      idQuery = 'SELECT * FROM student ORDER BY ID DESC;'
+      cr.execute(idQuery)
+      res = cr.fetchone()
+      print('inserted value is ',res )
 
-    
-    print('After insertion Submit book  Name are as ', curStudent[-1][0], curStudent[-1][1])
+            #.....................To ISSUE_BOOK from liberary..................
 
+   def issue_book(self):
 
+      db = sqlite3.connect ('liberary.db' )
+      cr = db.cursor()
 
-    # to add the books in liberary......
-def addBook():
-    adbk = ["Hindi","English","Math","Sanskrit"]
-    bookName = input(" Enter Book name : ")
-    bookPub = input("Enetr name of publication :")
-    book_id = int(input("Enter Book_id :"))
-    book_status =(input("Enter Book_status :")) 
-    print("Book detail is enter in library")
-    query = 'SELECT book_name, ID from add_book order by ID ASC;'
-    cursor.execute(query)
-    curStudent = cursor.fetchall()
-    id = curStudent[-1][1] + 1
-    for st in curStudent:
-        print(' BEfore Insertion Book Name are as ', st[0])
-    query = f'INSERT INTO add_book(ID, book_name, publication_name, book_id, book_status) VALUES (1, "{bookName}", "{bookPub}", {book_id}, "{book_status}")'
-    cursor.execute(query)
-    db.commit()
-    query = 'SELECT book_name, book_status from add_book order by ID ASC;'
-    cursor.execute(query)
-    curStudent = cursor.fetchall()
+      #creating TABLE for issue_book.........
+      # query = 'CREATE TABLE issue_book (ID INT PRIMARY KEY, booknm varchar(50), Issuedate date, Studentnm varchar(50), studentroll int(10));'
+      # cr.execute(query)
+      # db.commit()
+      # query = "DROP TABLE Issue_book"
+      
+      # cr.execute(query)
+      # db.commit()
+      
+      Booknm = input("Enter the name of book: ")
+      Issuedate = input("Enter the date of issue book :")
+      Studentnm = input("Enter the name of student :")
+      studentroll = int(input("Enter the roll of student :"))
 
-    
-    print('After insertion  book  Name are as ', curStudent[-1][0], curStudent[-1][1])
+      idQuery = 'SELECT ID FROM issue_book ORDER BY ID DESC;'
+      cr.execute(idQuery)
+      res = cr.fetchone()
+
+      #Query To Insert Data in issue_book Table[(0,9,8)]
+      #try:
+      print(res[0])
+      newID =res[0]+ 1
+      query = f'INSERT INTO issue_book VALUES({newID},"{Booknm}","{Issuedate}","{Studentnm}",{studentroll}); '
+      cr.execute(query)
+      db.commit()
+      print('insert value is ',res)
 
 
-""" End """
-addBook()
-submitBook()
-issue_book()
-addStudent()
+      idQuery = 'SELECT * FROM issue_book ORDER BY ID DESC;'
+      cr.execute(idQuery)
+      res = cr.fetchone()
+      print('inserted value is ',res) 
+
+
+
+            #.......................To SUBMIT_BOOK in liberary.........................
+
+   def submitbook(self):
+      db = sqlite3.connect ('liberary.db' )
+      cr = db.cursor()
+   #creating TABLE for submitbook
+      # query = 'CREATE TABLE submitbook(ID INT PRIMARY KEY ,DOS int(20) , bookName varchar(50) , studentNAME varchar(50) , studentRoll int );'
+      # cr.execute(query)
+      # db.commit()
+      # query = "DROP TABLE submitbook"
+      # cr.execute(query)
+      # db.commit()
+
+      DOS = input("Date of submission : ")
+      bookName = input("Enter Book Name : ")
+      studentName = input("enter the name of student :")
+      studentroll = int(input("Enter the roll of student :"))
+
+      idQuery = 'SELECT  ID FROM submitbook ORDER BY ID DESC;'
+      cr.execute(idQuery)
+      res = cr.fetchone()
+      #Query To Insert Data in submitbook Table
+      
+      print(res[0])
+      newID =res[0]+ 1
+      query = f'INSERT INTO submitbook(ID , DOS , bookName , studentName , studentroll) VALUES({newID} , "{DOS}" , "{bookName}" , "{studentName}" , {studentroll});'
+      cr.execute(query)
+      db.commit()
+      print('insert value is ',res)
+
+      idQuery = 'SELECT * FROM submitbook ORDER BY ID DESC;'
+      cr.execute(idQuery)
+      res = cr.fetchone()
+      print('inserted value is ',res)
+
+
+               #................ to ADD_BOOK in liberary..................
+
+   def addBook(self):
+
+      db = sqlite3.connect ('liberary.db' )
+      cr = db.cursor()
+   #Creating table for addbook
+      # query = 'CREATE TABLE addbook (ID INT PRIMARY KEY, Name varchar(100), bookPub varchar(100), book_id INT, book_status varchar(100));'
+      # cr.execute(query)
+      # db.commit()
+
+      bookName = input(" Enter Book name : ")
+      bookPub = input("Enetr name of publication :")
+      book_id = int(input("Enter Book_id :"))
+      book_status =int(input("Enter Book_status :")) 
+
+      idQuery = 'SELECT  ID FROM addbook ORDER BY ID DESC;'
+      cr.execute(idQuery)
+      res = cr.fetchone()
+      # Query TO Insert Data in addbook Table 
+      print(res)
+      newID = res[0]+1
+      query = f'INSERT INTO addbook VALUES({newID},"{bookName}", "{bookPub}" ,{book_id},{book_status});' 
+      cr.execute(query)
+      db.commit()
+
+      idQuery = 'SELECT * FROM addbook ORDER BY ID DESC;'
+      cr.execute(idQuery)
+      res = cr.fetchone()
+      print('inserted value is ',res)
+
+obj = Liberary()
+
+# callFunction()........   
+# obj.addStudent()
+#obj.issue_book()
+obj.submitbook()
+obj.addBook()
+db.close()
