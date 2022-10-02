ca73eef77ca2260bc01d544ae9a1aaaed43d9c20
ankitasingh20081998 <109068867+ankitasingh20081998@users.noreply.github.com>
2022-10-02 17:41:48 +0530

    update class

--- digliapp/diglib.py
+++ digliapp/diglib.py
@@ -1,22 +1,12 @@
 class book:
-    def __init__(self,book_name,authorname,publication_name,pages,price,weight,height,length):
-      self.book_name =book_name
-      self.authorname =authorname
-      self.publication_name =publication_name
-      self.pages = pages
-      self.price =price
-      self.weight = weight
-      self.height =height
-      self.length = length
+    def __init__(self):
+        self.book_isbn = ""
+        self.book_title = ""
+        self.book_subject = ""
+        self.book_publisher = ""
+        self.book_number_of_pages = ""
+        self.book_authors = ""
+        self.book_edition = ""
+        self.book_barcode = ""
 
-    def Author(self):
-      a_name = input("Enter the name of Author: ")
-      a_id = int(input("Enter the id of the Author: "))
-      a_address = input("Enter the address of Author: ")
-      a_phone = int(input("Enter the phone number of the Author:"))
-      a_email = input("Enter the email of the Author:")
-      
-      
- 
-      
-  
+    
